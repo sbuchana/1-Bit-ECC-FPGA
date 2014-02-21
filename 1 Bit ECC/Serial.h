bfb3c4e6d957f9a53bd242955ecc9e1c16b243d5
Sandy Buchanan <s.buchanan@alumni.ubc.ca>
2014-02-20 18:57:39 -0800

    Adding existing serial connection files

---
+++ 1 Bit ECC/Serial.h
@@ -0,0 +1,52 @@
+// Modified: February 20 2014
+//                            
+// This code is courtesy of www.arduino.cc public wiki
+// Modified to meet my requirements
+//
+// serial connection class header
+// Serial.h
+
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+class Serial
+{
+    private:
+        //Serial comm handler
+        HANDLE hSerial;
+
+        //Connection status
+        bool connected;
+
+        //Get various information about the connection
+        COMSTAT status;
+
+        //Keep track of last error
+        DWORD errors;
+
+    public:
+        //Initialize Serial communication with the given COM port
+        Serial(LPCSTR portName);
+
+        //Close the connection
+        //NOTA: for some reason you can't connect again before exiting
+        //the program and running it again
+        ~Serial();
+
+        //Read data in a buffer, if nbChar is greater than the
+        //maximum number of bytes available, it will return only the
+        //bytes available. The function return -1 when nothing could
+        //be read, the number of bytes actually read.
+        int ReadData(char *buffer, unsigned int nbChar);
+
+        //Writes data from a buffer through the Serial connection
+        //return true on success.
+        bool WriteData(char *buffer, unsigned int nbChar);
+
+        //Check if we are actually connected
+        bool IsConnected();
+
+};
